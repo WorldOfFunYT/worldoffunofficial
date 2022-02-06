8f45afa6330508065ef849e74b2c5817fc218f27
World Of Fun <czarczynskiszymon@gmail.com>
2022-02-06 10:13:26 +0000

    Made blog work

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var postdata = getPosts();
+window.onload = function() {
+postlist = document.querySelector("#postlist")
+if (getQueryVariable("postid") == false) {
+  listAllPosts(postlist);
+  console.log("has not found postid variable")
+}
+if (! getQueryVariable("postid") == false) {
+  postdata.then((value) => {
+    postlist.innerHTML = value.Posts[getQueryVariable("postid")].title;
+    console.log("found postid variable")
+  })
+} else {
+  
+}
+}
 const nav = document.querySelector("#header");
 fetch("header.html")
   .then((res) => res.text())
@@ -26,22 +42,47 @@ function findByKey(key, value) {
   return (item, i) => item[key] === value;
 }
 
-var postdata = getPosts("title", "another title");
-postdata.then((value) => {
-  console.log(value, "hi");
-});
 
-function getPosts(param, val) {
-  return fetch("posts.json")
-    .then((response) => response.json())
-    .then((data) => {
-      var postdata = data.Posts;
-      console.log(postdata);
-      let findParams = findByKey(param, val);
-      let index = postdata.findIndex(findParams);
-      return postdata;
-    });
+
+async function getPosts() {
+  const response = await fetch("posts.json");
+  const data = await response.json();
+  return data;
 }
+postdata.then((value) => {
+  console.log(value)
+})
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+function listAllPosts(container) {
+  postdata.then((value) => {
+    for (let i = 0; i < value.Posts.length; i++) {
+      var id = value.Posts[i].id - 1;
+      var title = value.Posts[i].title;
+      var author = value.Posts[i].author;
+      var image = value.Posts[i].img;
+      var date = value.Posts[i].pubDate;
+      var time = value.Posts[i].pubTime;
+      var content = value.Posts[i].content;
+      console.log(id, title, author, image, date, time, content);
+      var code = '<a href="?postid=' + id + '"><h2>' + title + '</h2></a><h3>Posted by ' + author + ' on ' + date + ' at ' + time + '</h3><img src="' + image + '" /><p>' + content + '</p>'
+      container.innerHTML += code;
+    }
+    }
+)
+  }
+
+
 // function showAboutPost() {
 //   var postId = 0;
 //   var postBox = document.getElementById("postbox");
@@ -67,4 +108,4 @@ function getPosts(param, val) {
 //         data.Posts[postId].content +
 //         "</p></div>";
 //     });
-// }
+//  }
